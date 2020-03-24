58653604b883c9e040284411a643d136d058ac41
flintpnz <kirill@kopylov.org>
2020-03-24 21:35:29 +0300

    Update topup.py
    v 0.2 реализовано пополнение по списку номеров CSV

--- topup.py
+++ topup.py
@@ -33,9 +33,11 @@ class TopUpState(StatesGroup):
     summa = State()
     popolnenie = State()
     balance = State()
+    csv_prep = State()
+    csv = State()
 
-@dp.message_handler(lambda message: message.text == 'Помощь', state='*')
-@dp.message_handler(lambda message: message.text == 'Начать сначала', state='*')
+@dp.message_handler(Text(equals='Помощь', ignore_case=True), state='*')
+@dp.message_handler(Text(equals='Начать сначала', ignore_case=True), state='*')
 @dp.message_handler(commands='help', state='*')
 @dp.message_handler(commands='start', state='*')
 async def cmd_start(message: types.Message, state: FSMContext):
@@ -43,7 +45,7 @@ async def cmd_start(message: types.Message, state: FSMContext):
     await TopUpState.start.set()
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.add("Начать", "Баланс")
+    markup.add("Начать", "CSV", "Баланс")
     markup.add("История", "Помощь")
     markup.add("Начать сначала")
 
@@ -58,12 +60,12 @@ async def cmd_start(message: types.Message, state: FSMContext):
         parse_mode=ParseMode.MARKDOWN,
     )
 
-@dp.message_handler(lambda message: message.text == 'Начать', state=TopUpState.start)
+@dp.message_handler(Text(equals='Начать', ignore_case=True), state=TopUpState.start)
 async def cmd_topup_start(message: types.Message):
     await TopUpState.numbers.set()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add('Начать сначала')
-    await message.answer('Введите список номеров в формате:\nНомер без 8/7/+7 (10 знаков)\nКаждый номер с новой строки', reply_markup=markup)
+    await message.answer('Введите список номеров в международном формате:\nНомер без знака +, начиная с 7 (11 знаков)\nКаждый номер с новой строки', reply_markup=markup)
 
 @dp.message_handler(state=TopUpState.numbers)
 async def cmd_topup_numbers(message: types.Message, state: FSMContext):
@@ -73,7 +75,7 @@ async def cmd_topup_numbers(message: types.Message, state: FSMContext):
     spisok = message.text.split('\n')
 
     for element in spisok:
-        if not re.match("^[7][9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$", element):
+        if not re.match("^[7][9][0-9]{9}$", element):
             return await message.reply('Неверный формат номеров, введите еще раз')
             
     spisok1 = '\n'.join(spisok)
@@ -151,6 +153,7 @@ async def cmd_topup_popolnenie(message: types.Message, state: FSMContext):
         )
         await message.answer('Процесс пополнения запущен, номера будут пополнены в течение 10 минут.', reply_markup=markup)
         for element in spisok:
+            #токен получен 24.03.2020, действует 3 года
             headers = {'Authorization': 'Bearer 41001106156656.6882AEA312238A159B3C9A555322F0800994AF9DB79C9899D07C254C8C78723E3988D67E7F4F84BBFBECFB32B4A5EE1E029BF838BF4CCECC4C465F6F5A83E2E01CC1EC30CB4B6FFDE4ECC3D29AAFB2C0EA97F4DE8C0AA08E92F0A4C77E7C4417F0C09E87B2E0A1F7238E9ACE6856188B119FCFB5A2ECD08526AD53812E1B9FB9'}
             payload = {'pattern_id':'phone-topup', 'phone-number':element, 'amount':proxy['summa'], 'test_payment':'true', 'test_result':'success'}
             r = requests.post('https://money.yandex.ru/api/request-payment', data=payload, headers=headers)
@@ -180,6 +183,67 @@ async def cmd_topup_popolnenie(message: types.Message, state: FSMContext):
     await message.answer('Процесс пополнения завершен.', reply_markup=markup)
     await state.finish()
 
+@dp.message_handler(Text(equals='CSV', ignore_case=True), state='*') # Вход на CSV пополнение
+async def cmd_csv_intro(message: types.Message, state: FSMContext):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Начать сначала')
+    await message.answer('Введите список номеров в формате CSV:\n79001111111,1.20\n79002222222,2.31\nСумма пополнения от 0.01 до 99.99', reply_markup=markup)
+    await TopUpState.csv_prep.set()
+
+@dp.message_handler(state=TopUpState.csv_prep) # Валидация списка CSV
+async def cmd_csv_validation(message: types.Message, state: FSMContext):
+    from csv import reader
+
+    csv_list = reader(message.text.split('\n'), delimiter=',')
+    for row in csv_list:
+        if not re.match("^[7][9][0-9]{9}$", row[0]):
+            msg = row[0] + ' - неверный формат номера, введите еще раз'
+            return await message.answer(msg)
+        if not re.match("^[0-9]{1,2}[.][0-9]{1,2}$", row[1]):
+            msg = row[1] + ' - неверный формат суммы пополнения, введите еще раз'
+            return await message.answer(msg)
+
+    msg = 'Список для пополнения:\n'
+    csv_list = reader(message.text.split('\n'), delimiter=',')
+    for row in csv_list:
+        msg += 'Номер: ' + row[0] + '\tСумма: ' + row[1] + '\n'
+    await message.answer(msg)
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Поехали!')
+    markup.add('Начать сначала')
+    await message.answer('Поехали?', reply_markup=markup)
+    await state.update_data(csv=message.text)
+    await TopUpState.csv.set()
+
+@dp.message_handler(Text(equals='Поехали!', ignore_case=True), state=TopUpState.csv) # Пополнение по CSV списку
+async def cmd_csv_payments(message: types.Message, state: FSMContext):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup.add('Начать сначала')
+    await message.answer('Начинаем пополнение.\nНомера будут пополнены в течение 10 минут.', reply_markup=markup)
+
+    from csv import reader
+    from json import loads
+    from requests import post
+
+    async with state.proxy() as proxy: # proxy = FSMContextProxy(state); await proxy.load()
+        csv_list = reader(proxy['csv'].split('\n'), delimiter=',')
+        for row in csv_list:
+            #токен получен 24.03.2020, действует 3 года
+            headers = {'Authorization': 'Bearer 41001106156656.6882AEA312238A159B3C9A555322F0800994AF9DB79C9899D07C254C8C78723E3988D67E7F4F84BBFBECFB32B4A5EE1E029BF838BF4CCECC4C465F6F5A83E2E01CC1EC30CB4B6FFDE4ECC3D29AAFB2C0EA97F4DE8C0AA08E92F0A4C77E7C4417F0C09E87B2E0A1F7238E9ACE6856188B119FCFB5A2ECD08526AD53812E1B9FB9'}
+            payload = {'pattern_id':'phone-topup', 'phone-number':row[0], 'amount':row[1], 'test_payment':'true', 'test_result':'success'} # тестовый запрос
+            #payload = {'pattern_id':'phone-topup', 'phone-number':row[0], 'amount':row[1]} # боевой запрос
+            r = post('https://money.yandex.ru/api/request-payment', data=payload, headers=headers)
+            if loads(r.text)['status'] == 'success':
+                msg = row[0] + ' пополнен успешно на сумму ' + row[1]
+                await message.answer(msg)
+            else:
+                msg = row[0] + ' НЕ ПОПОЛНЕН на сумму ' + row[1]
+                await message.answer(msg)                
+
+    await message.answer('Пополнение номеров завершено!')
+    await state.finish()
+
 @dp.message_handler(lambda message: message.text == 'Баланс', state='*')
 async def cmd_balance(message: types.Message):
     #await TopUpState.balance.set()
@@ -188,14 +252,14 @@ async def cmd_balance(message: types.Message):
     markup.add('Начать сначала')
     await message.answer('Ваш баланс: 0.00р', reply_markup=markup)
 
-@dp.message_handler(lambda message: message.text == 'Пополнить баланс', state='*')
+@dp.message_handler(Text(equals='Пополнить баланс', ignore_case=True), state='*')
 async def cmd_balance_add(message: types.Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add('Начать сначала')
     await message.answer('Введите сумму пополнения (целое число с шагом 1р)', reply_markup=markup)
     await TopUpState.balance.set()
 
-@dp.message_handler(lambda message: message.text == 'Да', state=TopUpState.balance)
+@dp.message_handler(Text(equals='Да', ignore_case=True), state=TopUpState.balance)
 async def cmd_balance_do(message: types.Message, state: FSMContext):
     #await TopUpState.popolnenie.set()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
@@ -203,13 +267,9 @@ async def cmd_balance_do(message: types.Message, state: FSMContext):
     await message.answer('Здесь будет ссылка на пополнение, или TG пополнение.', reply_markup=markup)
     await state.finish()
 
-# Check age. Age gotta be digit
 @dp.message_handler(lambda message: not message.text.isdigit(), state=TopUpState.balance)
 #@dp.message_handler(lambda message: not float(message.text), state=TopUpState.balance)
 async def process_age_invalid(message: types.Message):
-    """
-    If age is invalid
-    """
     return await message.answer('Введите сумму пополнения (целое число с шагом 1р)')
 
 @dp.message_handler(lambda message: message.text.isdigit(), state=TopUpState.balance)
@@ -224,7 +284,6 @@ async def cmd_balance_start(message: types.Message):
     markup.add('Да')
     markup.add('Начать сначала')
 
-    # And send message
     await bot.send_message(
         message.chat.id,
         md.text(
@@ -236,29 +295,16 @@ async def cmd_balance_start(message: types.Message):
         parse_mode=ParseMode.MARKDOWN,
     )
 
-@dp.message_handler(lambda message: message.text == 'История', state='*')
+@dp.message_handler(Text(equals='История', ignore_case=True), state='*')
 async def cmd_history(message: types.Message):
     #await TopUpState.balance.set()
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     markup.add('Начать сначала')
-    await message.answer('Здесь будем выводить историю пополнений', reply_markup=markup)
+    await message.answer('Здесь будем выводить историю последних 5 пополнений', reply_markup=markup)
 
-# You can use state '*' if you need to handle all states
-@dp.message_handler(state='*', commands='cancel')
-@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
-    """
-    Allow user to cancel any action
-    """
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-
-    logging.info('Cancelling state %r', current_state)
-    # Cancel state and inform user about it
-    await state.finish()
-    # And remove keyboard (just in case)
-    await message.reply('Cancelled.', reply_markup=types.ReplyKeyboardRemove())
+@dp.message_handler(state='*') # Неизвестное состояние
+async def unknown(message: types.Message, state: FSMContext):
+    await message.reply('Попробуй еще раз')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
